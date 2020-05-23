0e97392ee598db1d8e681a70bc4e6f0bdbafc79a
madosuki <yurikasamaaikatsu@gmail.com>
2020-05-23 23:43:23 +0900

    delete test.lisp

--- test.lisp
+++
@@ -1,4 +0,0 @@
-(ql:quickload :generate-like-certain-board-strings)
-
-(defun main ()
-  (print (generate-like-certain-board-strings:dat-to-html "/home/kouf/Documents/common_lisp/like-the-certain-board/dat/1588232341.dat")))
