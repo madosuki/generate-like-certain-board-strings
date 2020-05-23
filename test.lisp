28a9efa8f21a0e229a4ebc982b5302d7f68a3d46
madosuki <yurikasamaaikatsu@gmail.com>
2020-05-23 17:14:25 +0900

    fix dat-to-html function

---
+++ test.lisp
@@ -0,0 +1,4 @@
+(ql:quickload :generate-like-certain-board-strings)
+
+(defun main ()
+  (print (generate-like-certain-board-strings:dat-to-html "/home/kouf/Documents/common_lisp/like-the-certain-board/dat/1588232341.dat")))
